3d05f162fc0e12ff9d4140febe22c287fad9fb54
oliviahylam <lamo273@newschool.edu>
2025-03-06 10:17:31 -0500

    Commit

--- Studio/workshops/penny-presser/scripts.js
+++ Studio/workshops/penny-presser/scripts.js
@@ -1,5 +1,6 @@
 // get the press button
 var pressButton = document.querySelector('button[name="press"]');
+var studentSelect= document.querySelector('select[name="student"]');
 var pennySelect = document.querySelector('select[name="penny"]');
 var pennyImage = document.querySelector('.penny');
 
@@ -9,11 +10,15 @@ console.log(pressButton);
 //add event listener to the press button
 pressButton.addEventListener('click', () => {
 
+    //log the student value
+    console.log(studentSelect.value);
+
     // log penny value
     console.log(pennySelect.value);
    
     //set the penny image source
-    pennyImage.src = 'img/' + pennySelect.value;
+    //pennyImage.src = 'img/' + pennySelect.value;
+    pennyImage.src = 'https://' + studentSelect.value + '.interactive.rodeo/studio/workshops/penny-presser/img/' + pennySelect.value;
 
      //log penny image
      console.log(pennyImage);
